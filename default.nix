b8472dee6263bd3a30faafd5594a1d479d948a38
Tiantian Li <i@arcohol.com>
2025-08-03 18:00:29 +0200

    build: use cargoLock

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ rustPlatform.buildRustPackage {
   pname = "majsoul-stats";
   version = "0.1.0";
   src = ./.;
-  cargoHash = "sha256-DYnkSI0r4W6ywzJH5F5kSaKPtCb1aDnwrQ2Lme6bsNM=";
+  cargoLock.lockFile = ./Cargo.lock;
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
 }
